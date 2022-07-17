a0945ed0d16b209a26d45a2b3326e324f59fee25
Touch-Night <102669562+Touch-Night@users.noreply.github.com>
2022-07-17 18:42:35 +0800

    Update WaystoneBlock.java

--- src/main/java/net/arathain/charter/block/WaystoneBlock.java
+++ src/main/java/net/arathain/charter/block/WaystoneBlock.java
@@ -15,6 +15,7 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
@@ -24,7 +25,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class WaystoneBlock extends Block implements BlockEntityProvider {
     public static final VoxelShape SHAPE = createCuboidShape(2, 0, 2, 14, 16, 14);
@@ -76,7 +76,7 @@ public class WaystoneBlock extends Block implements BlockEntityProvider {
         }
         super.onBroken(world, pos, state);
     }
-    @Override
+    
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         super.randomDisplayTick(state, world, pos, random);
         if(state.get(Properties.LIT)) {
