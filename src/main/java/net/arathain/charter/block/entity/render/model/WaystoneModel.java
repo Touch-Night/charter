c38c9909ed99136b6684a0b1a959c8f276755d6b
Touch-Night <102669562+Touch-Night@users.noreply.github.com>
2022-07-17 18:32:28 +0800

    Update WaystoneModel.java

--- src/main/java/net/arathain/charter/block/entity/render/model/WaystoneModel.java
+++ src/main/java/net/arathain/charter/block/entity/render/model/WaystoneModel.java
@@ -5,6 +5,7 @@ import net.arathain.charter.block.entity.WaystoneEntity;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
+@Override
 public class WaystoneModel extends AnimatedGeoModel<WaystoneEntity> {
     private static final Identifier TEXTURE_IDENTIFIER = new Identifier(Charter.MODID, "textures/block/waystone.png");
     private static final Identifier MODEL_IDENTIFIER = new Identifier(Charter.MODID, "geo/waystone.geo.json");
@@ -20,7 +21,7 @@ public class WaystoneModel extends AnimatedGeoModel<WaystoneEntity> {
         return TEXTURE_IDENTIFIER;
     }
 
-    @Override
+    
     public Identifier getAnimationFileResource(WaystoneEntity animatable) {
         return ANIMATION_IDENTIFIER;
     }
