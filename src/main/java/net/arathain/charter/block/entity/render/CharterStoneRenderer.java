f3858053a8ef45824de3b64bfc971e85369318f5
Touch-Night <102669562+Touch-Night@users.noreply.github.com>
2022-07-17 18:33:55 +0800

    Update CharterStoneRenderer.java

--- src/main/java/net/arathain/charter/block/entity/render/CharterStoneRenderer.java
+++ src/main/java/net/arathain/charter/block/entity/render/CharterStoneRenderer.java
@@ -14,7 +14,7 @@ import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
-import java.awt.Color
+import java.awt.Color;
 
 public class CharterStoneRenderer extends GeoBlockRenderer<CharterStoneEntity> {
     AnimatedGeoModel<CharterStoneEntity> stone = new CharterStoneModel();
